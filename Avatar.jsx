69569ea3edcbb0de40918f673471d8860df9cb46
csalvadorarnau <c.salvador.arnau@gmail.com>
2026-09-01 16:39:39 +0200

    Delete Avatar.jsx

--- Avatar.jsx
+++
@@ -1,4 +0,0 @@
-export default function Avatar({ initial, size = 'md' }) {
-  const cls = size === 'sm' ? 'avatar sm' : size === 'xs' ? 'avatar xs' : 'avatar'
-  return <div className={cls}>{initial}</div>
-}
